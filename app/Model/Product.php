e897ce5307fc46cfe4204277e555061ffa9ec14a
Yan Aung <yanaung85@gmail.com>
2019-08-11 11:50:15 +0800

    Relationship created between Product and review table

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -1,10 +1,18 @@
 <?php
 
 namespace App\Model;
+Use App\Model\Reivew;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+
+	/*One product may have many reviews*/
+    public function reviews(){
+
+    	return $this->hasMany(Review::class);
+    	
+    }
+
 }
